121aae6f40c70479f0a09d3ce2da2168ce9dbb3e
Gary Rendle <gary.444@hotmail.co.uk>
2017-10-21 17:16:13 +0200

    string unfinished

--- Strings/src/Addition.java
+++ Strings/src/Addition.java
@@ -6,9 +6,24 @@ public class Addition {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+        int sum;
 		
 		//todo: Read integer values as input and add them until the user writes a 's'.
 		//Print the result.
+        
+        System.out.println("Enter integer values or press 's' to print the sum:");
+        
+        String input = scan.next();
+        
+        while (input != "s"){
+            
+            int num = parseInt(input);
+            
+            sum += num;
+        }
+        
+        
+        System.out.println("Sum = " + sum);
 		
 	}
 	
