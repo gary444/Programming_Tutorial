5b3b51fc06f1edcb4dc4c301cd3b76661f452e32
Gary Rendle <gary.444@hotmail.co.uk>
2017-10-21 18:01:48 +0200

    string ex 1 done

--- Strings/src/Addition.java
+++ Strings/src/Addition.java
@@ -6,20 +6,26 @@ public class Addition {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-        int sum;
+        int sum = 0;
 		
 		//todo: Read integer values as input and add them until the user writes a 's'.
 		//Print the result.
         
         System.out.println("Enter integer values or press 's' to print the sum:");
-        
         String input = scan.next();
         
-        while (input != "s"){
-            
-            int num = parseInt(input);
-            
-            sum += num;
+        while (!input.equals("s")){
+
+            try {
+                int num = Integer.parseInt(input);
+                sum += num;
+            }
+            catch (NumberFormatException e){
+                System.out.println("Invalid Response.");
+            }
+
+
+            input = scan.next();
         }
         
         
